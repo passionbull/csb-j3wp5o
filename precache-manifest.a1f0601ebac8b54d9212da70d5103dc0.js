ada097dd954ea6be3ae71418d1ed655185107a34
Code Sandbox <hello@codesandbox.io>
2023-02-04 20:45:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a1f0601ebac8b54d9212da70d5103dc0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5e66a0047ef817dc117babd36b7483",
+    "url": "https://passionbull.github.io/csb-j3wp5o/index.html"
+  },
+  {
+    "revision": "5f7a46da8f467e89a92d",
+    "url": "https://passionbull.github.io/csb-j3wp5o/static/js/2.e751248c.chunk.js"
+  },
+  {
+    "revision": "3f7418440edf0335f9b6",
+    "url": "https://passionbull.github.io/csb-j3wp5o/static/js/main.ff98e157.chunk.js"
+  },
+  {
+    "revision": "ffd233bb898eb1508737",
+    "url": "https://passionbull.github.io/csb-j3wp5o/static/js/runtime~main.95bb0e78.js"
+  }
+]);
